9571430360057215b50fdcb9b79227d859cad96e
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-12-03 22:29:33 -0300

    .

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -28,7 +28,7 @@ const HeroSection = () => {
                             </Span>
                         </TextLoop>
                         <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='display'>CV</ResumeButton>
+                        <ResumeButton href={Bio.resume} target='display'>Curriculum Vitae</ResumeButton>
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
